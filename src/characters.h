8c29f34102ee8a01db4a505276794dd72411ce03
Tuğra Güldoğan <tugraguldogan44@gmail.com>
2021-07-27 22:00:21 +0300

    Create characters.h

---
+++ src/characters.h
@@ -0,0 +1,23 @@
+  
+#ifndef CHAR_H
+#define CHAR_H
+
+extern void charFrame();
+
+#include "common.h"
+#include <stdbool.h>
+#include <SDL_opengl.h>
+
+extern float playerX;
+extern float playerY;
+
+extern bool movingRight;
+extern bool movingLeft;
+extern bool jumpTry;
+
+void spawnChar(Coord c);
+void resetControls(void);
+void applyControlsToPlayer(void);
+void initCharacters();
+
+#endif
